875fe244f8896016312d734b0448d14fc432b6dc
Thibaud Christe <thibaud.christe@he-arc.ch>
2020-03-19 11:08:47 +0100

    Section 13 - Observable

--- Section13/obs-01-start/src/app/home/home.component.ts
+++ Section13/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private firstObsSubscription: Subscription;
+
   constructor() { }
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe( count => {
+    //  console.log(count);
+    // });
+
+    const customIntervalObservable = Observable.create(observer => {
+        let count = 0;
+        setInterval( () => {
+          observer.next(count);
+          if ( count == 2) {
+            observer.complete();
+          }
+          if ( count > 3) {
+            observer.error(new Error('Error is greater than 3'));
+          }
+          count++;
+        }, 1000)
+    });
+
+    this.firstObsSubscription =   customIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data:number) => {
+      return 'Round : ' + (data + 1);
+    })).subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message)
+    }, () => {
+      console.log('Completed');
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
