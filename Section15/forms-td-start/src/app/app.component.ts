04dc4ec29a0fdb1a9f22407b05b053faa7508f45
Thibaud Christe <thibaud.christe@he-arc.ch>
2020-03-26 20:44:30 +0100

    Section 15 - Using form data + reset form

--- Section15/forms-td-start/src/app/app.component.ts
+++ Section15/forms-td-start/src/app/app.component.ts
@@ -11,18 +11,17 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-    // this.signUpForm.setValue({
-    //  userData: {
-    //    username: suggestedName,
-    //    email: ''
-    //  },
-    // secret: 'pet',
-    // questionAnswer: '',
-    //   gender: 'male'
-    // });
     this.signUpForm.form.patchValue({
       userData: {
         username: suggestedName
@@ -31,6 +30,13 @@ export class AppComponent {
   }
 
   onSubmit() {
-    console.log(this.signUpForm);
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+
+    this.signUpForm.reset();
   }
 }
