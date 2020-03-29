adf13861188352902af897a4aaee7f7d3388eda6
Thibaud Christe <thibaud.christe@he-arc.ch>
2020-03-29 12:50:02 +0200

    Section 15 - Reactive - Base code

--- Section15/forms-td-assignment-start/src/app/app.component.ts
+++ Section15/forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: false}) form: NgForm;
+  subscriptionTypes = ['Basic', 'Advanced', 'Pro'];
+  defaultSubscriptionType = 'Advanced';
+  data = {
+    email: '',
+    subscriptionType: '',
+    password: ''
+  };
+  submitted = false;
 
+  onSubmit() {
+    this.submitted = true;
+    this.data.email = this.form.value.email;
+    this.data.subscriptionType = this.form.value.subscriptionType;
+    this.data.password = this.form.value.password;
+    this.form.reset();
+  }
 }
