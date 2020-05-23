53a0fea1f2535fb01f34b3593ada442dbc0f2d49
Thibaud Christe <thibaud.christe@he-arc.ch>
2020-05-23 12:23:58 +0200

    Section 18 - Multiple Interceptors

--- Section18/http-02-post-requests/src/app/auth-interceptor.service.ts
+++ Section18/http-02-post-requests/src/app/auth-interceptor.service.ts
@@ -1,9 +1,8 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        console.log('Request is on its way');
-
         // Modifier une requête
         const modifiedRequest = req.clone({
             headers: req.headers.append('Auth', 'xyz')
