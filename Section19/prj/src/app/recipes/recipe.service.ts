19a68acef5f6171e1cda7348b67db30ba1cef2fa
Thibaud Christe <thibaud.christe@he-arc.ch>
2020-06-03 19:53:38 +0200

    Section 19 - Base code

---
+++ Section19/prj/src/app/recipes/recipe.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
+
+    private recipes: Recipe[] = [
+        new Recipe (
+            'Tasty Schniztel',
+            'A super-testy Schnitzel - just awesome !!',
+            'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
+        ),
+        new Recipe (
+            'Big Burger',
+            'What else you need to say ?',
+            'https://upload.wikimedia.org/wikipedia/commons/6/62/NCI_Visuals_Food_Hamburger.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ]
+        )
+    ];
+
+    constructor(private shoppingListService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+}
