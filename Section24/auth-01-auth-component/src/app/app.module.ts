376e5fe6eda8c11d2d14ecb950066b2b62f365e8
Thibaud Christe <thibaud.christe@he-arc.ch>
2021-04-02 15:27:53 +0200

    On avance

--- Section24/auth-01-auth-component/src/app/app.module.ts
+++ Section24/auth-01-auth-component/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+//import { StoreModule } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { LoggingService } from './logging.service';
+// import { ShoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { LoggingService } from './logging.service';
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    //StoreModule.forRoot({shoppingList: ShoppingListReducer})
   ],
   bootstrap: [AppComponent],
   // providers:[LoggingService]
