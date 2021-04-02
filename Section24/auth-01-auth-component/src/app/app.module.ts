0b4656ca5e3f403f5f7b68283160b1db349a1b78
Thibaud Christe <thibaud.christe@he-arc.ch>
2021-04-02 15:59:05 +0200

    upgrade bootstrap

--- Section24/auth-01-auth-component/src/app/app.module.ts
+++ Section24/auth-01-auth-component/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-//import { StoreModule } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,7 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { LoggingService } from './logging.service';
-// import { ShoppingListReducer } from './shopping-list/store/shopping-list.reducer';
+import { ShoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,7 @@ import { LoggingService } from './logging.service';
     AppRoutingModule,
     SharedModule,
     CoreModule,
-    //StoreModule.forRoot({shoppingList: ShoppingListReducer})
+    StoreModule.forRoot({shoppingList: ShoppingListReducer})
   ],
   bootstrap: [AppComponent],
   // providers:[LoggingService]
