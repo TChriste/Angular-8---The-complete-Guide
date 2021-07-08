642ab41fc3cd884be2929ecadc0a5c9180ddd31d
Thibaud Christe <thibaud.christe@he-arc.ch>
2021-07-08 15:23:12 +0200

    Section 24 - Store - Auth

--- Section24/auth-01-auth-component/src/app/header/header.component.ts
+++ Section24/auth-01-auth-component/src/app/header/header.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer'
 
 @Component({
   selector: 'app-header',
@@ -12,10 +15,14 @@ export class HeaderComponent implements OnInit, OnDestroy{
   isAuthenticated = false;
   private userSub: Subscription;
 
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
+  ) {}
 
   ngOnInit() {
-    this.userSub = this.authService.user.subscribe(user => {
+    this.userSub = this.store.select('auth').pipe(map(authState => {return authState.user;})).subscribe(user => {
       this.isAuthenticated = !!user; // !user ? false : true; = !!user
     });
   }
--- Section24/auth-01-auth-component/src/app/shopping-list/store/shopping-list.actions.ts
+++ Section24/auth-01-auth-component/src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,12 +1,12 @@
 import { Action } from '@ngrx/store';
 import { Ingredient } from '../../shared/ingredient.model';
 
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
-export const UPDATE_INGREDIENTS = 'UPDATE_INGREDIENTS';
-export const DELETE_INGREDIENTS = 'DELETE_INGREDIENTS';
-export const START_EDIT = 'START_EDIT';
-export const STOP_EDIT = 'STOP_EDIT';
+export const ADD_INGREDIENT = '[Shpping List] Add Ingredient';
+export const ADD_INGREDIENTS = '[Shpping List] Add Ingredients';
+export const UPDATE_INGREDIENTS = '[Shpping List] Update Ingredient';
+export const DELETE_INGREDIENTS = '[Shpping List] Delete Ingredient';
+export const START_EDIT = '[Shpping List] Start Edit';
+export const STOP_EDIT = '[Shpping List] Stop Edit';
 
 
 export class AddIngredient implements Action {
